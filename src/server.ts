f4ed3bc108d6569ccc8e1712bc053b4b339472a6
Bibin A <bibinaurify@gmail.com>
2025-08-27 19:30:25 +0530

    Server log changed

--- src/server.ts
+++ src/server.ts
@@ -10,6 +10,6 @@ const PORT = process.env.PORT || 5000;
 // Connect to database and start server
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running: http://localhost:${PORT}`);
   });
 });

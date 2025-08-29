1d055636889f604a834fde25b70e7fe291e70d3e
Bibin A <bibinaurify@gmail.com>
2025-08-29 14:25:58 +0530

    scheme active and deleted field added

--- src/models/scheme.ts
+++ src/models/scheme.ts
@@ -5,29 +5,36 @@ export interface IScheme extends Document {
   months: number;
   amount: number;
   bounce: number;
+  is_active: boolean;
+  is_deleted: boolean;
 }
 
 const schemeSchema = new Schema<IScheme>(
-    {
-        name: {
-            type: String,
-            required: [true, "Name is required"]
-        },
-        months:
-        {
-            type: Number,
-            required: true
-        },
-        amount:
-        {
-            type: Number,
-            required: true
-        },
-        bounce:
-        {
-            type: Number,
-            required: true
-        },
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    months: {
+      type: Number,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+    bounce: {
+      type: Number,
+      required: true,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+    is_deleted: {
+      type: Boolean,
+      default: false,
+    }
   },
   {
     timestamps: true,

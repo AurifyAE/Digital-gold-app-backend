20559e004f7b644be04359d34f2f1c0ad443720f
Bibin A <bibinaurify@gmail.com>
2025-08-29 14:20:01 +0530

    Session model for token

---
+++ src/models/session.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface ISession extends Document {
+  user_id: Types.ObjectId;
+  token: string;
+  createdAt: Date;
+}
+
+const SessionSchema: Schema<ISession> = new Schema(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    token: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Session = mongoose.model<ISession>("Session", SessionSchema);
+export default Session;

e690952a0ec32819a8be755d914ebf58da9e2ac2
Bibin A <bibinaurify@gmail.com>
2025-09-09 16:21:26 +0530

    User wallet model

---
+++ src/models/wallet.ts
@@ -0,0 +1,28 @@
+import mongoose, { Document, Schema, Types } from "mongoose";
+
+export interface IWalletScheme extends Document {
+  user_id: Types.ObjectId;
+  balance: number;
+}
+
+const walletSchema: Schema = new Schema<IWalletScheme>(
+  {
+    user_id: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+      min: 0
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const wallet = mongoose.model<IWalletScheme>("Wallet", walletSchema);
+
+export default wallet;

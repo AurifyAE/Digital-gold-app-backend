66a127f37e8e5beb9f54d543eaf1050864585efd
Bibin A <bibinaurify@gmail.com>
2025-09-10 15:32:05 +0530

    Aim and scheme user payment API's removed

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,10 +2,8 @@ import express from "express";
 import { listSchemes } from "../controllers/user/scheme/list";
 import { selectScheme } from "../controllers/user/scheme/select";
 import { selectedSchemesList } from "../controllers/user/scheme/selectedSchemesList";
-import { payment } from "../controllers/user/scheme/payment";
 import { addAim } from "../controllers/user/aim/add";
 import { listAims } from "../controllers/user/aim/list";
-import { aimPayment } from "../controllers/user/aim/payment";
 import { addAddress } from "../controllers/user/address/add";
 import { getAddress } from "../controllers/user/address/get";
 import { updateAddress } from "../controllers/user/address/update";
@@ -21,12 +19,10 @@ const router = express.Router();
 router.get("/scheme", listSchemes);
 router.post("/scheme", selectScheme);
 router.get("/scheme-selected", selectedSchemesList);
-router.post("/scheme/payment", payment);
 
 // Aim routes
 router.post("/aim", validateAim, addAim);
 router.get("/aim", listAims);
-router.patch("/aim/payment", aimPayment);
 
 // Address routes
 router.post("/address", validateAddress, addAddress);

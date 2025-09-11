fbeceddf813fde4d798f78ada3d677632ab0615b
Bibin A <bibinaurify@gmail.com>
2025-09-11 10:28:47 +0530

    Payment request list for wallet

--- src/controllers/admin/payment/list.ts
+++ src/controllers/admin/payment/list.ts
@@ -9,57 +9,28 @@ export const listPayments = async (req: Request, res: Response) => {
         localField: "user_id",
         foreignField: "_id",
         as: "user",
+        pipeline: [
+          {
+            $project: {
+              first_name: 1,
+              last_name: 1,
+              mobile_no: 1,
+              email: 1,
+            },
+          },
+        ],
       },
     },
     { $unwind: "$user" },
     {
-      $lookup: {
-        from: "selectedschemes",
-        localField: "selected_scheme_id",
-        foreignField: "_id",
-        as: "selected_scheme",
-      },
-    },
-    { $unwind: "$selected_scheme" },
-    {
-      $lookup: {
-        from: "schemes",
-        localField: "selected_scheme.scheme_id",
-        foreignField: "_id",
-        as: "scheme",
-      },
-    },
-    { $unwind: "$scheme" },
-    // Final projection for clean structure
-    {
       $project: {
         _id: 1,
+        transaction_id: 1,
         paid_amount: 1,
         status: 1,
         paidAt: 1,
-        // User details
-        user: {
-          first_name: "$user.first_name",
-          last_name: "$user.last_name",
-          mobile_no: "$user.mobile_no",
-          email: "$user.email",
-        },
-        // SelectedScheme details
-        selected_scheme: {
-          _id: "$selected_scheme._id",
-          balance_payout: "$selected_scheme.balance_payout",
-          payment_date: "$selected_scheme.payment_date",
-          status: "$selected_scheme.status",
-        },
-        // Scheme details
-        scheme: {
-          _id: "$scheme._id",
-          name: "$scheme.name",
-          months: "$scheme.months",
-          amount: "$scheme.amount",
-          monthly_pay: "$scheme.monthly_pay",
-          bonus: "$scheme.bonus",
-        },
+        payment_type: 1,
+        user: 1,
       },
     },
   ]);

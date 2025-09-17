6d3445e53a85a533a9c6507be1daa2d78f777a84
Bibin A <bibinaurify@gmail.com>
2025-09-17 17:00:00 +0530

    All scheduler will run every day at 12AM in one job

--- src/cron/index.ts
+++ src/cron/index.ts
@@ -8,52 +8,18 @@ import { schemePaymentScheduler } from "./schemePaymentScheduler";
 
 class JobScheduler {
   static initializeJobs() {
-    // Create daily aim payments for active aims
-    // Run everyday at 12:00 AM
     cron.schedule(
+      // Run everyday at 12:00 AM
       "0 0 * * *",
       () => {
         aimPaymentsDailyScheduler();
-        console.log("Creating daily aim payments");
-      },
-      { timezone: "Asia/Kolkata" }
-    );
-
-    // Create weekly aim payments for active aims
-    // Run every day at 12:00 AM
-    cron.schedule(
-      "0 0 * * *",
-      () => {
         aimPaymentsWeeklyScheduler();
-        console.log("Creating weekly aim payments");
-      },
-      { timezone: "Asia/Kolkata" }
-    );
-
-    // Create monthly aim payments for active aims
-    // Run every day at 12:00 AM
-    cron.schedule(
-      "0 0 * * *",
-      () => {
         aimPaymentsMonthlyScheduler();
-        console.log("Creating monthly aim payments");
-      },
-      {
-        timezone: "Asia/Kolkata",
-      }
-    );
-
-    // Create scheme payments for active schemes
-    // Run every day at 12:00 AM
-    cron.schedule(
-      "0 0 * * *",
-      () => {
         schemePaymentScheduler();
-        console.log("Creating scheme payments");
+        console.log("All schemes and aims payments created...");
       },
       { timezone: "Asia/Kolkata" }
     );
-
     console.log("✅ Cron jobs initialized successfully");
   }
 }

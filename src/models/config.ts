bbf0eb01aaab3d849f4e751596f3c22810194758
Bibin A <bibinaurify@gmail.com>
2025-09-23 12:19:53 +0530

    config schema modified

--- src/models/config.ts
+++ src/models/config.ts
@@ -3,6 +3,8 @@ import mongoose, { Document, Schema } from "mongoose";
 export interface IConfig extends Document {
     key: string;
     value: any;
+    is_active: boolean;
+    is_deleted: boolean;
 }
 
 const configSchema: Schema = new Schema<IConfig>(
@@ -15,6 +17,14 @@ const configSchema: Schema = new Schema<IConfig>(
         value: {
             type: mongoose.Schema.Types.Mixed,
             required: true,
+        },
+        is_active: {
+            type: Boolean,
+            default: true,
+        },
+        is_deleted: {
+            type: Boolean,
+            default: false,
         },
     },
     {

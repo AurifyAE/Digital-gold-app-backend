a72efba29504e78af5fa60277599e866d8b1257a
Bibin A <bibinaurify@gmail.com>
2025-09-23 12:04:12 +0530

    Add public static folder

--- src/app.ts
+++ src/app.ts
@@ -12,6 +12,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 // Morgan logging middleware
 if (process.env.NODE_ENV === "development") {

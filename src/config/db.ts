12c20eb74b30b88c49f0b4e6d7a8abd6a54845e4
Bibin A <bibinaurify@gmail.com>
2025-09-25 16:59:16 +0530

    Test log added

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const connectDB = async () => {
+  console.log("MongoDB URI===>", process.env.MONGODB_URI);
   const mongoURI = process.env.MONGODB_URI;
   if (!mongoURI) {
     throw new Error("MONGO_URI is not defined in environment variables");
